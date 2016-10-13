8b1f26adc99f8753a5044b4df061b83b587d390a
隼人 <takebnn26@gmail.com>
2016-10-13 11:22:57 +0900

    popular機能

--- app/controllers/prototypes/popular_controller.rb
+++ app/controllers/prototypes/popular_controller.rb
@@ -1,7 +1,7 @@
-class PopularController < ApplicationController
+class Prototypes::PopularController < ApplicationController
 
   def index
-    @prototypes = Prototype.order("likes_count DESC").includes(:user)
+    @prototypes = Prototype.order("likes_count DESC").includes(:user).page(params[:page]).per(2)
   end
 
 end
